8ee1f8ef61e3088f6ffa77ff49c91ecf97cae278
qianjin <qjlhlh@gmail.com>
2015-10-19 13:55:33 +0800

    1

--- app/src/main/java/com/androidbase/presenter/ArticlePresenter.java
+++ app/src/main/java/com/androidbase/presenter/ArticlePresenter.java
@@ -1,41 +1,34 @@
 package com.androidbase.presenter;
 
-import com.androidbase.data.http.HttpHelper;
-import com.androidbase.data.http.HttpResultHandler;
-import com.androidbase.entity.Page;
-import com.androidbase.entity.Result;
-import com.androidbase.model.ArticleModel;
-import com.androidbase.view.iview.IBaseView;
-
 /**
  * Created by qianjin on 2015/9/25.
  */
 public class ArticlePresenter {
 
-    private ArticleModel articleModel;
-    private IBaseView articleView;
-
-    public ArticlePresenter(IBaseView articleView){
-        this.articleView = articleView;
-        articleModel = new ArticleModel();
-    }
-
-    public void getArticles(final Page page){
-//        articleModel.getArticles(page, new HttpResultHandler() {
+//    private ArticleModel articleModel;
+//    private IBaseView articleView;
+//
+//    public ArticlePresenter(IBaseView articleView){
+//        this.articleView = articleView;
+//        articleModel = new ArticleModel();
+//    }
+//
+//    public void getArticles(final Page page){
+////        articleModel.getArticles(page, new HttpResultHandler() {
+////            @Override
+////            public void onSuccess(Result result) {
+////                articleView.getDataSuccess(result);
+////            }
+////        });
+//
+//        HttpHelper.getArticleList(page, new HttpResultHandler() {
 //            @Override
 //            public void onSuccess(Result result) {
 //                articleView.getDataSuccess(result);
 //            }
 //        });
-
-        HttpHelper.getArticleList(page, new HttpResultHandler() {
-            @Override
-            public void onSuccess(Result result) {
-                articleView.getDataSuccess(result);
-            }
-        });
-
-
-    }
+//
+//
+//    }
 
 }
